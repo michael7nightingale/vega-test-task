03a729944e363e6f52c6bc24d7a57abd2b4e4390
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-21 23:28:43 +0500

    connect new events to application factory function

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-from core.events import startup_handler
+from core.events import startup_handler, register_routers, use_authentication_middleware
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
-
+    register_routers(app)
+    use_authentication_middleware(app)
     app.add_event_handler("startup", startup_handler(app))
-
     return app

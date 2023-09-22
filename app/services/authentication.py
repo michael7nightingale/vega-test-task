baa32df713c148c7176a3504128420f50c4c97ec
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-22 10:36:44 +0500

    add password hashing

--- app/services/authentication.py
+++ app/services/authentication.py
@@ -1,4 +1,5 @@
 from jose import jwt, JWTError
+from passlib.hash import sha256_crypt
 from datetime import datetime, timedelta
 from pydantic import ValidationError
 
@@ -30,3 +31,11 @@ def decode_jwt_token(encoded_token: str) -> Token | None:
         return Token(**payload)
     except (JWTError, ValidationError):
         return
+
+
+def hash_password(password: str) -> str:
+    return sha256_crypt.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return sha256_crypt.verify(password, hashed_password)

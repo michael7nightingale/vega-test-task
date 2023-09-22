9e8272c99fd4514c4e215908989cdf0803abc974
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-22 11:39:03 +0500

    refactor for docker

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+import os
 
-from app.core.events import startup_handler, register_routers, use_authentication_middleware
+from app.core.events import (
+    startup_handler,
+    register_routers,
+    use_authentication_middleware,
+    use_production_settings,
+
+)
 
 
-def create_app() -> FastAPI:
+def create_app(*args, **kwargs) -> FastAPI:
+    """Application factory function."""
     app = FastAPI()
+
+    if os.getenv("PROD"):   # checks if to use production environment
+        use_production_settings()
+
     register_routers(app)
     use_authentication_middleware(app)
     app.add_event_handler("startup", startup_handler(app))
+
     return app

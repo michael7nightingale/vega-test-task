516ac51b0fa135abdda857567cf572b483c60b6b
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-22 23:55:01 +0500

    create watch getting dependency

---
+++ app/api/dependencies/watches.py
@@ -0,0 +1,16 @@
+from fastapi import HTTPException, Request
+
+from app.models.watches import Watch
+
+
+async def get_watch(request: Request, watch_id: str):
+    watch = await Watch.get_or_none(id=watch_id, user=request.user)
+    if watch is None:
+        raise HTTPException(
+            detail=f"Watch {watch_id} for user with id={request.user.id} does not exist.",
+            status_code=404
+        )
+    if not watch.is_watched:
+        watch.is_watched = True
+        await watch.save()
+    return watch

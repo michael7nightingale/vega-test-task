88ab84fd8f02a14b31e4b366c28b0b34e69e404b
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-23 12:07:15 +0500

    update dependencies

--- app/api/dependencies/watches.py
+++ app/api/dependencies/watches.py
@@ -1,8 +1,10 @@
 from fastapi import HTTPException, Request
 
-from app.models.watches import Watch
+from app.models.watches import Watch, Task
+from app.services.authentication import login_required
 
 
+@login_required
 async def get_watch(request: Request, watch_id: str):
     watch = await Watch.get_or_none(id=watch_id, user=request.user)
     if watch is None:
@@ -14,3 +16,14 @@ async def get_watch(request: Request, watch_id: str):
         watch.is_watched = True
         await watch.save()
     return watch
+
+
+@login_required
+async def get_task(request: Request, watch_id: str, task_id: str):
+    task = await Task.get_or_none(id=task_id, watch__user=request.user, watch_id=watch_id)
+    if task is None:
+        raise HTTPException(
+            detail=f"Task {task_id} for user with id={request.user.id} does not exist.",
+            status_code=404
+        )
+    return task
